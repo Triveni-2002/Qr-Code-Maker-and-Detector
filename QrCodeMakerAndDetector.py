60d709bceb64535b4b6814e3194334fb1a2fb39e
Triveni Dande <82858526+Triveni-2002@users.noreply.github.com>
2022-04-22 07:57:52 +0530

    Update QrCodeMakerAndDetector.py

--- QrCodeMakerAndDetector.py
+++ QrCodeMakerAndDetector.py
@@ -1,8 +1,9 @@
 import qrcode
 import cv2
-img=qrcode.make("www.google.com")
+s=input()
+img=qrcode.make(s)
 img.show()
 img.save("yoda.jpg")
 d=cv2.QRCodeDetector()
 val,points,s=d.detectAndDecode(cv2.imread("yoda.jpg"))
-print(val)
+print("Decoding value for a given string from Qrcode:",val)
